e850c7d54163fc33a315c2f1f7e919606a9fbced
garrett-hunt <garretthunt1@outlook.com>
2024-10-06 21:02:49 -0500

    Add functionality to light output

--- src/Components/MorseInput/MorseInput.tsx
+++ src/Components/MorseInput/MorseInput.tsx
@@ -20,25 +20,113 @@ const MorseInput: React.FC<MorseInputProps> = ({
   translate,
 }) => {
   const [morseOutputType, setMorseOutputType] = useState<MorseType>('TEXT');
+  const [morseLight, setMorseLight] = useState<Boolean>(false);
+  const [lightValue, setLightValue] = useState('Click to Play');
   const morseTypes: MorseType[] = ['TEXT', 'LIGHT', 'SOUND'];
+
+  const playLightTranslation = async (output: string) => {
+    const newCharPause = () => {
+      return new Promise<void>((resolve) => {
+        setTimeout(() => {
+          setMorseLight(false);
+          resolve();
+        }, 300);
+      });
+    };
+
+    const lightDot = () => {
+      return new Promise<void>((resolve) => {
+        setTimeout(() => {
+          setMorseLight(false);
+          resolve();
+        }, 100);
+        newCharPause();
+        setMorseLight(true);
+      });
+    };
+
+    const lightDash = () => {
+      return new Promise<void>((resolve) => {
+        setTimeout(() => {
+          setMorseLight(false);
+          resolve();
+        }, 300);
+        newCharPause();
+        setMorseLight(true);
+      });
+    };
+
+    const pause = () => {
+      return new Promise<void>((resolve) => {
+        setTimeout(() => {
+          setMorseLight(false);
+          resolve();
+        }, 700);
+      });
+    };
+
+    setLightValue('');
+    const fixedOutput = output.replace('?', '');
+    await pause();
+    console.log(fixedOutput);
+
+    for (const char of fixedOutput) {
+      if (char === '.') {
+        await lightDot();
+        await newCharPause();
+        console.log('dot');
+      } else if (char === '-') {
+        await lightDash();
+        await newCharPause();
+        console.log('dash');
+      } else {
+        await pause();
+        console.log('space');
+      }
+    }
+    setLightValue('Click to Play');
+  };
 
   return (
     <div className="morseContainer">
       <div className="inputContainer">
         <h1>Morse Code</h1>
-        <textarea
-          disabled={isDisabled}
-          name="morseInput"
-          value={input}
-          placeholder={!isDisabled ? 'Input a value to be translated' : ''}
-          onChange={(event) => {
-            if (!isDisabled) {
-              const newInput = event.target.value;
-              setInput(newInput);
-              setOutput(() => translate(newInput));
-            }
-          }}
-        ></textarea>
+        {morseOutputType === 'TEXT' && (
+          <textarea
+            disabled={isDisabled}
+            name="morseInput"
+            value={input}
+            placeholder={!isDisabled ? 'Input a value to be translated' : ''}
+            onChange={(event) => {
+              if (!isDisabled) {
+                const newInput = event.target.value;
+                setInput(newInput);
+                setOutput(() => translate(newInput));
+              }
+            }}
+          ></textarea>
+        )}
+        {morseOutputType === 'LIGHT' && (
+          <textarea
+            className={'morseLight'}
+            name="morseInput"
+            readOnly={true}
+            value={lightValue}
+            style={{
+              backgroundColor: morseLight
+                ? 'rgb(255, 255, 0)'
+                : 'rgb(59, 59, 59)',
+            }}
+            onClick={() => {
+              playLightTranslation(input);
+            }}
+          >
+            Play
+          </textarea>
+        )}
+        {morseOutputType === 'SOUND' && (
+          <div>{/* Placeholder for sound output functionality */}</div>
+        )}
       </div>
       <div>
         {isDisabled
