7d9f605fb06ec5ab5c96b135eb09a7222a7da1ed
garrett-hunt <garretthunt1@outlook.com>
2025-05-24 22:44:46 -0500

    Fix light output component not working

--- src/Components/LightOutput/LightOutput.tsx
+++ src/Components/LightOutput/LightOutput.tsx
@@ -78,6 +78,7 @@ export const LightOutput: React.FC<LightOutputProps> = ({ output }) => {
           playLightTranslation(output);
         }
       }}
+      style={{ backgroundColor: morseLight ? 'yellow' : 'rgb(60, 60, 60)' }}
     >
       Play
     </textarea>
